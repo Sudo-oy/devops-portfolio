df32aaf8629822566a099b8d1530f38039a91d27
Sudo-oy <oy.sudodata@gmail.com>
2025-10-29 17:12:36 +0100

    Create alb.tf

---
+++ terraform/alb.tf
@@ -0,0 +1,30 @@
+# terraform/alb.tf
+resource "aws_lb" "devops_alb" {
+  name               = "devops-portfolio-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.devops_sg.id]
+  subnets            = aws_subnet.public[*].id
+
+  tags = {
+    Name = "devops-portfolio-alb"
+  }
+}
+
+resource "aws_lb_target_group" "devops_tg" {
+  name     = "devops-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+}
+
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.devops_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.devops_tg.arn
+  }
+}
